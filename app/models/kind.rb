eb97bd7660ab2e0e13bb5d229a3d89bce3613ad4
Nico Proto <nicolasproto@gmail.com>
2021-03-06 20:06:42 +0100

    Added validations and associations for kind and review models

--- app/models/kind.rb
+++ app/models/kind.rb
@@ -1,2 +1,8 @@
 class Kind < ApplicationRecord
+  CATEGORIES = %w[electric fire water grass flying poison bug normal ground].freeze
+
+  has_many :kinds_pokemons
+  has_many :pokemons, through: :kinds_pokemons
+
+  validates :name, inclusion: { in: CATEGORIES, message: "%{value} is not a valid class" }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :booking
+
+  validates :rating, inclusion: { in: 1..5, message: "Rating should be between 1 to 5" }
+  validates :content, length: { minimum: 25 }
 end
