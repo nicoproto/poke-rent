3afeee409ba8136dff5c77b14e6150977389fcb4
Nico Proto <nicolasproto@gmail.com>
2021-03-06 19:37:05 +0100

    Added associations, validations and class methods for bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,30 @@
 class Booking < ApplicationRecord
+  before_create :set_total_price
+
   belongs_to :pokemon
   belongs_to :user
+  has_one :review, dependent: :destroy
+
+  validates :start_date, :end_date, presence: true
+  validate :end_date_after_start_date
+
+  enum status: [ :pending, :accepted, :declined ]
+
+  private
+
+  def duration
+    @duration ||= (end_date - start_date).to_i
+  end
+
+  def set_total_price
+    self.total_price = duration * pokemon.price
+  end
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, 'End date must not be before start date')
+    end
+  end
 end
