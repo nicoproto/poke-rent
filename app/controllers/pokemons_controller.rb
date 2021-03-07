a7b6de09f78ffa33aa52cf79a3d3447e33ce95f5
Nico Proto <nicolasproto@gmail.com>
2021-03-07 17:47:02 +0100

    Added create and new actions for pokemon

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -4,4 +4,26 @@ class PokemonsController < ApplicationController
   def index
     @pokemons = Pokemon.all
   end
+
+  def new
+    @pokemon = Pokemon.new
+  end
+
+  def create
+    @pokemon = Pokemon.new(pokemon_params)
+    @pokemon.user = current_user
+
+    if @pokemon.save
+      redirect_to pokemon_path(@pokemon)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def pokemon_params
+    # TODO: Add kinds when ready
+    params.require(:pokemon).permit(:name, :description, :price, :location)
+  end
 end
