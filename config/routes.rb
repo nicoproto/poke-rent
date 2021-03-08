f2f5ac254038e4a9ba6ec8f66539dabc15fde07f
Nico Proto <nicolasproto@gmail.com>
2021-03-08 12:04:10 +0100

    Removed kinds from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :pokemons do
-    resources :kinds_pokemons, only: [ :new, :create ]
-  end
+  resources :pokemons
 end
