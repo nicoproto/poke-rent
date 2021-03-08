003819f455cc07ddac4640556d16d1142aa041c2
Nico Proto <nicolasproto@gmail.com>
2021-03-08 13:46:17 +0100

    Added create action and show route

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,4 +3,21 @@ class BookingsController < ApplicationController
     @pokemon = Pokemon.find(params[:pokemon_id])
     @booking = Booking.new
   end
+
+  def create
+    @pokemon = Pokemon.find(params[:pokemon_id])
+    @booking = Booking.new(booking_params)
+    @booking.pokemon = @pokemon
+    @booking.user = current_user
+
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :pokemons do
     resources :bookings, only: [:new, :create]
   end
-  resources :bookings, only: :destroy
+  resources :bookings, only: [:destroy, :show]
 end
