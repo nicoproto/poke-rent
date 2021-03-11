b389de76df134086c0a959f273b8d50ce87887ef
Nico Proto <nicolasproto@gmail.com>
2021-03-11 18:17:17 +0100

    Added starts to edit too

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -1,5 +1,4 @@
 import "jquery-bar-rating";
-import $ from 'jquery'; // <-- if you're NOT using a Le Wagon template (cf jQuery section)
 
 const initStarRating = () => {
   $('#review_rating').barrating({
