31d29845d75e105a263ec3f21314b141d7ebf7c7
Nico Proto <nicolasproto@gmail.com>
2021-03-12 17:08:11 +0100

    Added pokedex to show and started show styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'kitchensink', to: 'pages#kitchensink'
+
   resources :pokemons do
     resources :bookings, only: [:new, :create]
   end
