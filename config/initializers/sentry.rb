3f3bbb857013b5d5e476352592e0a812ebf05f89
Nico Proto <nicolasproto@gmail.com>
2021-08-12 17:44:48 +0200

    FEAT: Configured sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,7 +2,7 @@ Sentry.init do |config|
   config.dsn = ENV['SENTRY_DSN']
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-  config.environments = %[production]
+  config.environments = %[production staging]
   # Set tracesSampleRate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production
